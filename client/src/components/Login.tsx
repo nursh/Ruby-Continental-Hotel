8971cc0074c4eb6e9ad7dcda9d815bb869b8b9e4
nursh <nsheikhmohammed@yahoo.com>
2019-04-03 17:32:20 -0400

    Finished with login form validation and bare bones code

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { withFormik, FormikProps, Form, Field, ErrorMessage } from 'formik';
+import { NavLink } from 'react-router-dom';
+import * as yup from 'yup';
 
 interface LoginFormProps {
   employeeNo: string,
@@ -9,6 +11,8 @@ interface LoginFormProps {
 const LoginForm = (props: FormikProps<LoginFormProps>) => {
   return (
     <div>
+      <h2>Ruby Continental</h2>
+      <h3>Hotel &amp; Suites</h3>
       <Form>
         <div>
           <Field name="employeeNo" placeholder="Employee No" />
@@ -18,6 +22,10 @@ const LoginForm = (props: FormikProps<LoginFormProps>) => {
         <div>
           <Field type="password" name="password" placeholder="Password" />
           <ErrorMessage name="password" />
+        </div>
+
+        <div>
+          <NavLink to="/reset">Reset Password</NavLink>
         </div>
 
         <button type="submit">Login</button>
@@ -30,6 +38,10 @@ export const Login = withFormik({
   mapPropsToValues: () => ({
     employeeNo: '',
     password: ''
+  }),
+  validationSchema: yup.object().shape({
+    employeeNo: yup.string().required('Employee No. is required'),
+    password: yup.string().required('Password is required')
   }),
   handleSubmit: ({ employeeNo, password}) => {
     console.log(`Employee No is ${employeeNo}`, `Password is ${password}`);
