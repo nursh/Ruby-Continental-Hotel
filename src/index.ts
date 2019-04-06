81b5b8b45c5a357ac6ce04e43d8d67b982752482
nursh <nsheikhmohammed@yahoo.com>
2019-04-06 04:39:45 -0400

    Added helmet and cors to express setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
-
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Working now'})
